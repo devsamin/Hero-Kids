eda9022607705aaf3491d28114982bde68220b76
Abdulla Al Samin <abdullahalsamin79@gmail.com>
2026-05-03 22:16:37 -1200

    learn when use clien

--- src/components/home/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import { BanglaFont } from "@/app/layout";
-import Image from "next/image";
-import React from "react";
-
-export default function Banner() {
-  return (
-    <div className="flex flex-col md:flex-row items-center justify-between gap-10 py-10"></div>
-  );
-}
