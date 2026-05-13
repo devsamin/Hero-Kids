f28b071b9558f2f3717af12ebbe5464e576a43ea
Abdulla Al Samin <abdullahalsamin79@gmail.com>
2026-05-12 20:58:30 -1200

    build up

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.MONGODB_URL;
+const uri = process.env.MONGODB_URI;
 const dbname = process.env.DB_NAME;
 export const collactions = {
   PRODUCT: "products",
